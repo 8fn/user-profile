e3cf90c8dd835d5b52135f1e08dd6741d9e30c72
Tanjil <tanjilkh@gmail.com>
2022-12-10 11:20:24 +0000

    :zap: Layout and routes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Templating engine
+const handlebars = exphbs.create({extname: '.hbs'})
+
+app.engine('hbs', handlebars.engine)
+app.set('view engine', 'hbs')
+
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
